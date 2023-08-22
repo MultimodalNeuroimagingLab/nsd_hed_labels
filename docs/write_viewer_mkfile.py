d52c1d143f652ab6c39715680bf2eaec5f7e80f3
Claire Holmes <136387637+holmesclaire@users.noreply.github.com>
2023-08-22 13:18:18 -0500

    fix syntax typos

--- docs/write_viewer_mkfile.py
+++ docs/write_viewer_mkfile.py
@@ -5,7 +5,7 @@ import pandas
 from mdutils.mdutils import MdUtils
 
 # load tsv of image identifiers and annotations
-shared1000 = pandas.read_table('../shared1000_HED.tsv')
+shared1000 = pandas.read_table('././shared1000_HED.tsv')
 
 # start markdown file
 mdFile = MdUtils(file_name='nsd_images_annotations', title='Images and Annotations', author= 'Claire Holmes')
